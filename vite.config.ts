40699a418ed0d0e0c3ce84e7dd20c3c03831b1bd
Deveta-Off <theo.grenet.pro@gmail.com>
2025-09-30 11:56:15 +0200

    chore: Removed netlify build files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-import viteReact from '@vitejs/plugin-react'
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tanstackStart(), netlify(), viteReact()],
+  plugins: [react(), tailwindcss()],
 });
